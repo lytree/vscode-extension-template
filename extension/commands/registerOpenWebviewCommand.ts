1f79501e5c3f825c5748f79d14a307150821b295
lytree <hi.yangzh@outlook.com>
2026-04-15 15:30:32 +0800

    fix error

--- extension/commands/registerOpenWebviewCommand.ts
+++ extension/commands/registerOpenWebviewCommand.ts
@@ -1,9 +1,9 @@
 import * as vscode from 'vscode';
 import { TemplatePanel } from '../panels/TemplatePanel.js';
 
-export function registerOpenWebviewCommand(context: vscode.ExtensionContext) {
+export function registerOpenWebviewCommand(context: vscode.ExtensionContext, cahannel: vscode.OutputChannel) {
   const disposable = vscode.commands.registerCommand('vscode-extension-template.openWebview', () => {
-    TemplatePanel.createOrShow(context.extensionUri);
+    TemplatePanel.createOrShow(context.extensionUri, cahannel);
   });
 
   context.subscriptions.push(disposable);
